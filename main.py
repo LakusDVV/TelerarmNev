d94503127be3c1d64441f303a486a3c07c5b0482
Lakus_DVV <evondvlad@gmail.com>
2025-05-14 12:46:01 +0300

    Add all project

---
+++ main.py
@@ -0,0 +1,50 @@
+import asyncio
+import dotenv
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+dotenv.load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+
+
+dp = Dispatcher()
+
+
+# Command handler
+@dp.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    await message.answer("Hello! I'm a bot created with aiogram.")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+
+
+
+# Run the bot
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
